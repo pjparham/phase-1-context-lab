267320c9a8f1a28a7f9d449ee3e99e25dfbce647
Parker Parham <parkerparham@gmail.com>
2022-08-09 16:25:05 -0400

    commit tree 30fcf27099100ac926e226ac42cc7f6ec8c3f9b3

--- index.js
+++ index.js
@@ -21,3 +21,78 @@ const allWagesFor = function () {
     return payable
 }
 
+function createEmployeeRecord(employeeArray){
+    return {
+        firstName: employeeArray[0],
+        familyName: employeeArray[1],
+        title: employeeArray[2],
+        payPerHour: employeeArray[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+}
+
+function createEmployeeRecords(employeeArrayofArrays){
+    return employeeArrayofArrays.map(employee => createEmployeeRecord(employee))
+}
+
+function createTimeInEvent(dateStamp){
+    let timeInEvent = {
+        type: 'TimeIn',
+        hour: parseInt(dateStamp.slice(11)),
+        date: dateStamp.slice(0,10)
+    } 
+    this.timeInEvents.push(timeInEvent)
+    return this
+}
+
+function createTimeOutEvent(dateStamp){
+    let timeOutEvent = {
+        type: 'TimeOut',
+        hour: parseInt(dateStamp.slice(11)),
+        date: dateStamp.slice(0,10)
+    } 
+    this.timeOutEvents.push(timeOutEvent)
+    return this
+}
+
+function hoursWorkedOnDate(dateStamp){
+    let employeeDate
+    let timeIn
+    let timeOut
+    for(let i = 0; i < this.timeInEvents.length; i++){
+        if (dateStamp == this.timeInEvents[i].date && dateStamp == this.timeOutEvents[i].date){
+            employeeDate = this.timeInEvents[i].date
+            timeIn = this.timeInEvents[i].hour
+            timeOut = this.timeOutEvents[i].hour
+        }
+    }
+    let rawHoursWorked = (timeOut - timeIn)
+    let rawHoursWorkedString = rawHoursWorked.toString()
+    let hoursWorkedString = rawHoursWorkedString.slice(0, -2)
+    let hoursWorked = (parseInt(hoursWorkedString))
+    return hoursWorked
+}
+
+function wagesEarnedOnDate(dateStamp){
+    let hoursWorked = hoursWorkedOnDate.call(this, dateStamp)
+    return hoursWorked*this.payPerHour
+}
+
+function findEmployeeByFirstName(employeeRecordArray, firstName){
+    // console.log(employeeRecordArray)
+    for (let i = 0; i < employeeRecordArray.length; i++){
+        if (firstName == employeeRecordArray[i].firstName){
+            return employeeRecordArray[i]
+        }
+        else{ return undefined}
+    }
+}
+
+function calculatePayroll(employeeRecordArray){
+    let allEmployeeWages = 0
+    for (let i = 0; i < employeeRecordArray.length; i++){
+        allEmployeeWages += allWagesFor.call(employeeRecordArray[i])
+    }
+    return allEmployeeWages
+}
